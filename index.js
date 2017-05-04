0d2fb7acc68b1ec646c08cc2d57db81d4ab2d402
Clara Kang <clara.kang91@gmail.com>
2017-05-03 17:20:54 -0700

    Created three puppies + action for reading all puppies + delete action on a single puppy

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const
   app = express(),
   bodyParser = require('body-parser'),
   morgan = require('morgan'),
-  mongoose = require('mongoose')
+  mongoose = require('mongoose'),
+  Puppy = require('./models/Puppy.js')
 
 //connecting to mongoose: database name: doghaus
   mongoose.connect('mongodb://localhost/doghaus', (err) => {
@@ -16,8 +17,26 @@ app.use(bodyParser.json())
 app.get('/', (req,res) => {
   res.json({message: "Who let the dogs out!"})
 })
+//SHOW all puppies
+app.get('/puppies', (req, res) => {
+  Puppy.find({}, (err, puppiesFromDb) => {
+    res.json(puppiesFromDb)
+  })
+})
 
+//CREATE puppies
+app.post('/puppies', (req, res) => {
+  Puppy.create(req.body, (err, newPuppy) => {
+    res.json({success: true, puppy: newPuppy})
+  })
+})
 
+//DELETE a puppy
+app.delete('/puppies/:id', (req, res) => {
+  Puppy.findByIdAndRemove(req.params.id, (err, deletedPuppy) => {
+    res.json({success: true, deletedPuppy})
+  })
+})
 //connecting the server
   app.listen(3000, (err) => {
     console.log(err || "Server running on 3000.")
