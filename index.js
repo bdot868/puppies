3a3f8c4645110a1c07d20fc801e9bd9045806402
Ahya <ayakurdi@gmail.com>
2017-05-03 17:25:46 -0700

    Added a route for all single puppies and a route to update puppies

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const
   bodyParser = require('body-parser'),
   morgan = require('morgan'),
   mongoose = require('mongoose')
+  Puppy = require('./models/Puppy.js')
 
 //connecting to mongoose: database name: doghaus
   mongoose.connect('mongodb://localhost/doghaus', (err) => {
@@ -16,7 +17,25 @@ app.use(bodyParser.json())
 app.get('/', (req,res) => {
   res.json({message: "Who let the dogs out!"})
 })
-
+// Finds all the puppies in the database
+app.get('/puppies', (req,res) => {
+  Puppy.find({},(err, AllDemPuppies) =>{
+  res.json(AllDemPuppies)
+  })
+})
+// Finds a single puppy using its ID (req.params.id)
+app.get('/puppies/:id',(req,res) =>{
+  Puppy.findById(req.params.id, (err,pup) =>{
+    res.json(pup)
+  })
+})
+// Finds a single puppy using its ID (req.params.id), updates the values (req.body)
+// and returns the new updated puppy (new:{true})
+app.patch('/puppies/:id', (req,res) =>{
+  Puppy.findByIDAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPup)=>{
+    res.json({sucess:true}, puppy:updatedPup)
+  })
+})
 
 //connecting the server
   app.listen(3000, (err) => {
