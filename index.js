d88207721169ad26268ae6cfb9ff13a379238e2a
Ahya <ayakurdi@gmail.com>
2017-05-04 15:37:17 -0700

    created routes for all controllers // deleted them from index.js page

--- index.js
+++ index.js
@@ -13,45 +13,12 @@ const
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
+app.use('/', require('./routes/puppies.js'))
 
 app.get('/', (req,res) => {
   res.json({message: "Who let the dogs out!"})
 })
 
-// Finds all the puppies in the database
-app.get('/puppies', (req,res) => {
-  Puppy.find({},(err, AllDemPuppies) =>{
-  res.json(AllDemPuppies)
-  })
-})
-// Finds a single puppy using its ID (req.params.id)
-app.get('/puppies/:id',(req,res) =>{
-  Puppy.findById(req.params.id, (err,pup) =>{
-    res.json(pup)
-  })
-})
-// Finds a single puppy using its ID (req.params.id), updates the values (req.body)
-// and returns the new updated puppy (new:{true})
-app.patch('/puppies/:id', (req,res) =>{
-  Puppy.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedPup)=>{
-    res.json({sucess:true, puppy: updatedPup})
-  })
-})
-
-
-//CREATE puppies
-app.post('/puppies', (req, res) => {
-  Puppy.create(req.body, (err, newPuppy) => {
-    res.json({success: true, puppy: newPuppy})
-
-  })
-})
-
-//DELETE a puppy
-app.delete('/puppies/:id', (req, res) => {
-  Puppy.findByIdAndRemove(req.params.id, (err, deletedPuppy) => {
-    res.json({success: true, deletedPuppy})
-  })
 })
 //connecting the server
 
---
+++ routes/puppies.js
@@ -0,0 +1,13 @@
+const
+  express = require('express'),
+  puppyRouter = express.Router(),
+  puppyCtrl = require('../controllers/puppies.js')
+
+puppyRouter.route('/puppies')
+  .get(puppyCtrl.index) //find all puppies
+  .post(puppyCtrl.create) //create a puppy
+
+puppyRouter.route('/puppies/:id')
+  .get(puppyCtrl.show) //show a single puppy
+  .patch(puppyCtrl.update) //update a puppy
+  .delete(puppyCtrl.destroy) //delete a puppy 
